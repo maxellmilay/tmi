0275f4ac0e62930165d0cc5adecbeae223e16e94
Maxell Milay <milaymaxell@gmail.com>
2023-01-20 15:50:54 +0800

    [Adjustment][Maxell] adjusted html tags

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -10,6 +10,6 @@ export default function SocialIcons(props: PropsInterface) {
     const { imgURL, alt } = props;
 
     return (
-        <Image src={imgURL} alt={alt} width={50} height={50} className="mr-2"></Image>
+        <Image src={imgURL} alt={alt} width={50} height={50} className="mr-2" />
     )
 }
