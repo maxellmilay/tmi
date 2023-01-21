58d37a602008fac42f35a2fecd011ed5aaf8c945
Wetooa <103910243+Wetooa@users.noreply.github.com>
2023-01-21 18:58:31 +0800

    fixed minor issue

--- components/TakeMeHomeButton.tsx
+++ components/TakeMeHomeButton.tsx
@@ -1,14 +1,12 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function TakeMeHomeButton() {
-	return (
-		<>
-			<Link
-				className="text-white bg-tmi-blue px-4 mt-6 rounded-xl w-fit"
-				href="/"
-			>
-				Take me home
-			</Link>
-		</>
-	);
+  return (
+    <Link
+      className="text-white bg-tmi-blue px-4 mt-6 rounded-xl w-fit"
+      href="/"
+    >
+      Take me home
+    </Link>
+  );
 }
