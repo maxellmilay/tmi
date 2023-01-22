be8f646bc1dae86bde304ec2eab546cd272a003d
Ezra Magbanua <magbanuaezra@gmail.com>
2023-01-22 15:32:51 +0800

    merge conflict

---
+++ components/Layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
+
+interface PropsInterface {
+	children?: JSX.Element | JSX.Element[];
+}
+
+function Layout(props: PropsInterface) {
+	const { children } = props;
+
+	return (
+		<div className="flex flex-col h-screen">
+			<Header />
+			<div className="flex-1">{children}</div>
+			<Footer />
+		</div>
+	);
+}
+
+export default Layout;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import Layout from '@/components/Layout';
+import React from 'react';
 
 export default function about() {
-    return (
-        <div className="flex flex-wrap items-center mt-20 mb-5">
-            <div className="flex w-1/2 h-1 bg-tmi-white" />
-            <p className="flex text-7xl font-bold grow justify-center">About Page</p>
-        </div>
-    )
+	return (
+		<Layout>
+			<p className="flex text-7xl font-bold grow justify-center">About Page</p>
+		</Layout>
+	);
 }
