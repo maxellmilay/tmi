c72360d01ee125496a933d73cdaa0eaa499e2a5a
Maxell Milay <milaymaxell@gmail.com>
2023-01-23 03:42:48 +0800

    [UI][Maxell] added statement section

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,12 +2,14 @@ import Layout from '@/components/Layout';
 import React from 'react';
 import History from '@/components/about/History';
 import Mission from '@/components/about/Mission';
+import Statement from '@/components/about/Statement';
 
 export default function about() {
 	return (
 		<Layout>
 			<History />
 			<Mission />
+			<Statement />
 		</Layout>
 	);
 }
