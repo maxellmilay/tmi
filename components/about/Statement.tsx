023ccb66a25b004a6b23900999a3dc8410a11d51
Wetooa <103910243+Wetooa@users.noreply.github.com>
2023-01-24 14:58:20 +0800

    [UI][Adrian] statement of faith backbone started

--- components/about/Statement.tsx
+++ components/about/Statement.tsx
@@ -1,28 +1,73 @@
-import React from 'react'
-import Section from '../Section'
-import StatementDetail from './StatementDetail'
+import React from "react";
+import Section from "../Section";
+import StatementDetail from "./StatementDetail";
 
 export default function Statement() {
-    return (
-        <>
-            <Section id='statement' className="flex items-end justify-end h-[20vh] md:h-[30vh] bg-[url('/images/about/mission-header.png')] no-repeat bg-cover bg-center">
-                <div className='flex items-center mb-5'>
-                    <h2 className='text-xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mr-3 sm:mr-7 md:mr-14'>Statement of Faith</h2>
-                    <div className='h-2 bg-white w-[40vw] md:w-[45vw]' />
-                </div>
-            </Section>
-            <Section className='flex flex-col items-center py-14 md:py-24 text-tmi-blue'>
-                <StatementDetail className='mb-16' info='We believe in One God, existing in three persons: The Father, the Son, the Holy Spirit' />
-                <StatementDetail className='mb-16' info='We believe in Jesus Christ, the begotten Son and eternally one with the Father, became incarnate by the Holy Spirit and was born by the Virgin Mary.' />
-                <StatementDetail className='mb-16' info='We believe in the Holy Spirit, eternally one with the Father and the Son, who is ever present and efficiently active in and with the church.' />
-                <StatementDetail className='mb-16' info='We believe in the inerrancy of the sixty-six books in the Bible in so far as what it teaches as written by men under the plenary inspiration of the Holy Spirit.' />
-                <StatementDetail className='mb-16' info='We believe in the depravity of men because of sin that came into the world.' />
-                <StatementDetail className='mb-16' info='We believe that Jesus Christ suffered, died, and is resurrected for the redemption of the fallen man.' />
-                <StatementDetail className='mb-16' info='We believe that the depraved man, by repentance, can be saved through the atoning sacrifice of Jesus Christ on the cross.' />
-                <StatementDetail className='mb-16' info='We believe in the need for fellowship and corporate gathering of believers for worship, prayer, thanksgiving, and supplication.' />
-                <StatementDetail className='mb-16' info='We believe in the twin ordinances of the Lord’s Supper and Water Baptism prescribed in the Bible.' />
-                <StatementDetail className='mb-16' info='We believe that the Lord Jesus Christ will come again for the believers to forever be with Him.' />
-            </Section>
-        </>
-    )
+  return (
+    <>
+      <Section
+        id="statement"
+        className="flex items-end justify-end h-[20vh] md:h-[30vh] bg-[url('/images/about/mission-header.png')] no-repeat bg-cover bg-center"
+      >
+        <div className="flex items-center mb-5">
+          <h2 className="text-xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mr-3 sm:mr-7 md:mr-14">
+            Statement of Faith
+          </h2>
+          <div className="h-2 bg-white w-[40vw] md:w-[45vw]" />
+        </div>
+      </Section>
+      <Section className="flex flex-col items-center py-14 md:py-24 text-tmi-blue">
+        <StatementDetail
+          className="mb-16"
+          number="/images/about/numbers/1.png"
+          info="We believe in One God, existing in three persons: The Father, the Son, the Holy Spirit"
+        />
+        <StatementDetail
+          className="mb-16"
+          number="/images/about/numbers/2.png"
+          info="We believe in Jesus Christ, the begotten Son and eternally one with the Father, became incarnate by the Holy Spirit and was born by the Virgin Mary."
+        />
+        <StatementDetail
+          className="mb-16"
+          number="/images/about/numbers/3.png"
+          info="We believe in the Holy Spirit, eternally one with the Father and the Son, who is ever present and efficiently active in and with the church."
+        />
+        <StatementDetail
+          className="mb-16"
+          number="/images/about/numbers/4.png"
+          info="We believe in the inerrancy of the sixty-six books in the Bible in so far as what it teaches as written by men under the plenary inspiration of the Holy Spirit."
+        />
+        <StatementDetail
+          className="mb-16"
+          number="/images/about/numbers/5.png"
+          info="We believe in the depravity of men because of sin that came into the world."
+        />
+        <StatementDetail
+          className="mb-16"
+          number="/images/about/numbers/6.png"
+          info="We believe that Jesus Christ suffered, died, and is resurrected for the redemption of the fallen man."
+        />
+        <StatementDetail
+          className="mb-16"
+          number="/images/about/numbers/7.png"
+          info="We believe that the depraved man, by repentance, can be saved through the atoning sacrifice of Jesus Christ on the cross."
+        />
+        <StatementDetail
+          className="mb-16"
+          number="/images/about/numbers/8.png"
+          info="We believe in the need for fellowship and corporate gathering of believers for worship, prayer, thanksgiving, and supplication."
+        />
+        <StatementDetail
+          className="mb-16"
+          number="/images/about/numbers/9.png"
+          info="We believe in the twin ordinances of the Lord’s Supper and Water Baptism prescribed in the Bible."
+        />
+        <StatementDetail
+          className="mb-16"
+          number="/images/about/numbers/10.png"
+          info="We believe that the Lord Jesus Christ will come again for the believers to forever be with Him."
+        />
+      </Section>
+    </>
+  );
 }
--- components/about/StatementDetail.tsx
+++ components/about/StatementDetail.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 interface PropsInterface {
-    className?: string
-    info: string
+  className?: string;
+  info: string;
+  number: string;
 }
 
 export default function StatementDetail(props: PropsInterface) {
-    const { className, info } = props;
+  const { className, info, number } = props;
 
-    return (
-        <div className={`flex justify-center text-center w-[70vw] ${className}`}>
-            <p className='text-md sm:text-lg md:text-xl'>{info}</p>
-        </div>
-    )
+  return (
+    <div
+      className={`flex relative h-fit justify-center items-center text-center w-[70vw] ${className}`}
+    >
+      <Image
+        src={number}
+        height={50}
+        width={50}
+        alt="number"
+        className="absolute w-auto h-20"
+      />
+      <p className="text-md sm:text-lg md:text-xl">{info}</p>
+    </div>
+  );
 }
