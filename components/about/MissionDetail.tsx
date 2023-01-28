760ec866efaab13e10c37cb6b5cbca4702ca5e21
vinceAle <vincealemania18@gmail.com>
2023-01-28 19:39:45 +0800

    [Adjustment][Vince] about page responsiveness

--- components/about/MissionDetail.tsx
+++ components/about/MissionDetail.tsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from "react";
 
 interface PropsInterface {
-    className?: string
-    title: string
-    info: string
+  className?: string;
+  title: string;
+  info: string;
 }
 
 export default function MissionDetail(props: PropsInterface) {
-    const { className, title, info } = props;
+  const { className, title, info } = props;
 
-    return (
-        <div className={`flex flex-col md:flex-row justify-center items-center md:mr-[5vw] ${className}`}>
-            <h3 className='flex justify-center md:justify-end md:mr-10 font-bold w-[70vw] md:w-[35vw] text-4xl mb-3'>{title}</h3>
-            <p className='text-center md:text-left w-[70vw] md:w-[30vw] text-lg'>{info}</p>
-        </div>
-    )
+  return (
+    <div
+      className={`flex flex-col md:flex-row justify-center items-center md:mr-[5vw] ${className}`}
+    >
+      <h3 className="flex justify-center text-center md:justify-end md:mr-10 font-bold w-[70vw] md:w-[35vw] text-4xl mb-3">
+        {title}
+      </h3>
+      <p className="text-center md:text-left w-[70vw] md:w-[30vw] text-lg">
+        {info}
+      </p>
+    </div>
+  );
 }
--- components/about/StatementDetail.tsx
+++ components/about/StatementDetail.tsx
@@ -13,7 +13,7 @@ export default function StatementDetail(props: PropsInterface) {
     <div
       className={`flex relative h-fit justify-center items-center text-center w-[70vw] ${className}`}
     >
-      <p className="absolute -z-10 shadow-blue text-9xl opacity-70 font-garamond text-outlined text-white number-container">
+      <p className="absolute -z-10 shadow-blue text-9xl opacity-70 font-garamond text-outlined text-white number-container mb-8">
         {number}
       </p>
       <p className="text-md sm:text-lg md:text-xl">{info}</p>
