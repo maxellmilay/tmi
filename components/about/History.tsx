d391c5027545f068e320787a27d0ae75af1491f0
Ezra Magbanua <magbanuaezra@gmail.com>
2023-01-30 14:19:41 +0800

    [Adjustment][Ezra] mobile about

--- components/about/History.tsx
+++ components/about/History.tsx
@@ -18,14 +18,14 @@ export default function History() {
           </h1>
         </div>
       </Section>
-      <Section className="flex flex-col md:flex-row relative justify-start md:justify-start items-center h-[55vh] md:h-[80vh] bg-[url('/images/about/history-bg-1.png')] no-repeat bg-cover bg-center px-10">
+      <Section className="flex flex-col md:flex-row relative justify-start md:justify-start items-center md:h-[80vh] bg-[url('/images/about/history-bg-1.png')] no-repeat bg-cover bg-center px-10">
         <Image
           src={HistoryPic1}
           alt="1st history pic"
           className="w-[80vw] h-[65vw] md:w-[50vw] md:h-[80vh] object-cover absolute -top-8 md:bottom-10"
         />
         <div className="flex mt-[65vw] md:mt-0 justify-center md:justify-end md:items-center w-full h-full">
-          <p className="text-black h-[35vw] md:h-fit w-[80vw] md:max-w-[35vw] text-justify">
+          <p className="text-black md:h-fit w-[80vw] md:max-w-[35vw] text-justify mb-5">
             <span className="font-bold">MOVED WITH THE DESIRE</span> to make a
             difference in the lives of underprivileged children in the
             marginalized sector of society who are deprived of preschool
@@ -39,8 +39,8 @@ export default function History() {
           </p>
         </div>
       </Section>
-      <Section className="flex md:items-center justify-center md:justify-start h-[40vh] sm:h-[35vh] px-12 sm:px-20 bg-[url('/images/about/mobile-history-bg-2.png')] md:bg-[url('/images/about/history-bg-2.png')] no-repeat bg-cover bg-center">
-        <p className="md:max-w-[40vw] text-lg sm:text-2xl text-justify leading-8 md:leading-10 mt-24 md:mt-0">
+      <Section className="flex md:items-center justify-center md:justify-start h-[80vh] px-12 sm:px-20 bg-[url('/images/about/mobile-history-bg-2.png')] md:bg-[url('/images/about/history-bg-2.png')] no-repeat bg-cover bg-center">
+        <p className="md:max-w-[40vw] sm:text-2xl text-justify leading-8 md:leading-10 mt-24 md:mt-0">
           The following month, on March 10, 2018, at Parklane Hotel, TMI Mobile
           School Program (TMSP) was officially launched. The event was witnessed
           by friends and supporters representing various sectors of society.
