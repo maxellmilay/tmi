e1925c4f9d93b37f866c8dda6f2ec8aa72fc4f01
Wetooa <103910243+Wetooa@users.noreply.github.com>
2023-02-03 19:54:55 +0800

    [adjustment][Adrian] adjusted history font sizes

--- components/about/StatementDetail.tsx
+++ components/about/StatementDetail.tsx
@@ -11,9 +11,9 @@ export default function StatementDetail(props: PropsInterface) {
 
   return (
     <div
-      className={`flex relative h-fit justify-center items-center text-center w-[70vw] ${className}`}
+      className={`flex relative h-auto justify-center items-center text-center w-[70vw] ${className}`}
     >
-      <p className="absolute -z-10 shadow-blue text-7xl sm:text-9xl opacity-70 font-garamond text-outlined text-white number-container mb-8">
+      <p className="absolute -z-10 shadow-blue text-7xl sm:text-9xl opacity-70 font-garamond text-outlined text-white number-container">
         {number}
       </p>
       <p className="text-xs sm:text-lg md:text-xl">{info}</p>
